d4d62ec04c87e118781b555a4dc8020b532635a3
Michelle Wehr <michelle_wehr@icloud.com>
2021-08-20 22:34:13 -0400

    got longitude and latitude of city

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,34 @@
 var searchCityEl = document.getElementById("searchCity");
 var cityHeadingEl = document.querySelector(".cityNameDate");
 
-$(".searchBtn").on("click", getWeather)
+$(".searchBtn").on("click", getCity);
 
-function getWeather(city) {
+function getCity() {
     //show city and date in currentWeather div
     var date = moment().format("MM/DD/YYYY");
     var city = searchCityEl.value
-    cityHeadingEl.textContent = city + "(" + date + ")";
-    //format weather api url
-    var weatherApiUrl = ""
+    cityHeadingEl.textContent = city + " (" + date + ")";
+
+    //format weather api url to get long/lat
+    var weatherApiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=ed2f5e6646f77e93759b1b042975be69";
+
+    //make a request to the url 
+    fetch(weatherApiUrl).then(function(response) {
+       //if no 404 error/ response.ok = successful, if not alert if there is a 404 error
+       if(response.ok) {
+        response.json().then(function(data) {
+            console.log(data);
+            getWeather(data);
+        });
+    } else {
+        alert("Please enter a valid city.");
+    }
+})
+}
+
+function getWeather(city) {
+    var latitude = city[0].lat;
+    var longitude = city[0].lon;
+    console.log(latitude);
+    console.log(longitude);
 }
